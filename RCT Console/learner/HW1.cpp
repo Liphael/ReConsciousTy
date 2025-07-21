8bafd2069f0334a1df534eccc5f0a457932aae88
Liphael <1454540255@qq.com>
2025-07-21 17:00:28 +0800

    regular_updt

---
+++ RCT Console/learner/HW1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+/*
+* 头文件iostream 是C++标准库中的一个头文件；
+其目的是为了提供输入输出功能的支持。
+*/
+
+// using namespace std;  
+/*
+* 命名空间namespace 是一个将能指标识符（用于指定变量、函数、类等的名称）组织在一起的容器；
+其目的是为了避免命名冲突。
+在大型项目中，不同的库或模块可能会定义相同名称的变量或函数，使用命名空间可以将它们区分开来。
+在开头的直接调用命名空间的写法，将覆盖整个文件中的所有标识符，这是较为危险的；
+建议在函数中调用命名空间，或者明确使用的命名空间前缀。
+*/
+
+/*
+int main()
+{
+	using namespace std;
+	cout << "HW 1" << endl;
+	system("pause");
+	return 0;
+}
+
+
+int main()
+{
+	std::cout << "HW 2" << std::endl;
+	system("pause");
+	return 0;
+}
+
+
+int main()
+{
+	using namespace std;
+	int a = 10;
+	cout << "a = " << a << endl;
+	system("pause");
+	return 0;
+}
+
+
+int main()
+{
+#define days_per_week 7 // 宏定义常量
+	using namespace std;
+	cout << "There are " << days_per_week << " days in a week." << endl;
+	system("pause");
+	return 0;
+}
+
+int main()
+{
+	const int days_per_week = 7; // const定义常量
+	using namespace std;
+	cout << "There are " << days_per_week << " days in a week." << endl;
+	system("pause");
+	return 0;
+}
+*/
+
